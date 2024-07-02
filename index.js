8c130fe56588be36c10feaf879d093d4756168b2
Ricardo Guerra <RikkuG491@gmail.com>
2024-07-01 23:28:04 -0400

    Created content and starter code for Practice - Callback Functions

---
+++ index.js
@@ -0,0 +1,5 @@
+const movieTitles = ["The Giant Gila Monster", "Manos: The Hands Of Fate", "Time Chasers"];
+
+// Write your solution code for Task # 1 here!
+
+// Write your solution code for Task # 2 here!
